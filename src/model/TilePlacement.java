add1102245d31134f2b9c74494bcba50358a3059
Malina D <m.m.dutu@student.utwente.nl>
2025-01-20 11:00:30 +0100

    TilePlacement adjustments

--- src/model/TilePlacement.java
+++ src/model/TilePlacement.java
@@ -2,4 +2,29 @@ package model;
 
 public class TilePlacement extends TileAction{
 
+    //Constructs the TilePlacement object
+    public TilePlacement(String rackTile, int destinationSet, int destinationTileIndex){
+        this.rackTile = rackTile;
+        this.destinationSet = destinationSet;
+        this.destinationTileIndex = destinationTileIndex;
+    }
+
+    //Code makeMove method
+    public void makeMove(Table table, List<Tile> rack) throws GameException {
+        boolean onRack = false;
+        Tile tileToPlace = null;
+        for (Tile tile : rack) {
+            if (tile.toString().equals(rackTile)){
+                onRack = true;
+                tileToPlace = tile;
+                break;
+            }
+        }
+        if (onRack){
+            table.placeTile(destinationSet, destinationTileIndex, tileToPlace);
+            rack.remove(tileToPlace);
+        } else {
+            throw new GameException("Tile not in rack");
+        }
+    }
 }
